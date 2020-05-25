f8731b3529c02d11790d659d27e3f1971b4d046c
DIPAK PAL <dipakpal@in.ibm.com>
2020-05-25 17:01:46 +0530

    git push origin master

---
+++ src/main/java/application/SampleRestController.java
@@ -0,0 +1,18 @@
+package application;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/hello")
+public class SampleRestController {
+	
+	@GetMapping
+	public String getHello( ) {
+		return "Hello Spring-Boot!!!";
+	}
+
+}
